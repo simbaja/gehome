27214e09812cbb78c7ce91d190f31dc2a7f0f6d5
Jack Simbach <jack.simbach@gmail.com>
2021-06-15 15:24:30 -0400

    - added default values for ErdAdvantiumCookStatus

--- gehomesdk/erd/values/advantium/erd_advantium_cook_status.py
+++ gehomesdk/erd/values/advantium/erd_advantium_cook_status.py
@@ -2,15 +2,15 @@ from typing import NamedTuple, Optional
 from .advantium_enums import *
 
 class ErdAdvantiumCookStatus (NamedTuple):
-    cook_action: CookAction
-    cook_mode: CookMode
-    termination_reason: TerminationReason
-    preheat_status: PreheatStatus
-    temperature: int
-    power_level: int
-    door_status: DoorStatus
-    sensing_active: SensingActive
-    cooling_fan_status: CoolingFanStatus
-    oven_light_status: OvenLightStatus
-    warm_status: WarmStatus
-    raw_value: Optional[str]
+    cook_action: CookAction = CookAction.STOP
+    cook_mode: CookMode = CookMode.NO_MODE
+    termination_reason: TerminationReason = TerminationReason.UNKNOWN
+    preheat_status: PreheatStatus = PreheatStatus.NO_PREHEAT
+    temperature: int = 0
+    power_level: int = 0
+    door_status: DoorStatus = DoorStatus.CLOSED
+    sensing_active: SensingActive = SensingActive.INACTIVE
+    cooling_fan_status: CoolingFanStatus = CoolingFanStatus.OFF
+    oven_light_status: OvenLightStatus = OvenLightStatus.OFF
+    warm_status: WarmStatus = WarmStatus.OFF
+    raw_value: Optional[str] = None
