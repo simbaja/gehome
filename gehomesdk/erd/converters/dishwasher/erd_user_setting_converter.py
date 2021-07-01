2ee0bdd3bc0b9f7e03fca8d61ab398119c3e7284
Jack Simbach <jack.simbach@gmail.com>
2021-07-01 15:01:01 -0400

    - decoded more dishwasher settings

--- gehomesdk/erd/converters/dishwasher/erd_user_setting_converter.py
+++ gehomesdk/erd/converters/dishwasher/erd_user_setting_converter.py
@@ -1,5 +1,12 @@
-from gehomesdk.erd.values.dishwasher.erd_user_setting import ErdUserSetting, UserSetting
 import logging
+from gehomesdk.erd.values.dishwasher.erd_user_setting import (
+    ErdUserSetting, 
+    UserSetting, 
+    UserCycleSetting, 
+    UserWashTempSetting,
+    UserDryOptionSetting,
+    UserWashZoneSetting
+)
 from ..abstract import ErdReadOnlyConverter
 from ..primitives import *
 
@@ -17,9 +24,16 @@ class ErdUserSettingConverter(ErdReadOnlyConverter[ErdUserSetting]):
             i = erd_decode_int(value)
             
             return ErdUserSetting(
-                sound = UserSetting((i & 8388608) >> 6),
+                bottle_jet = UserSetting(i & 1),
+                cycle_mode = UserCycleSetting(i & 14 >> 1),
+                sabbath = UserSetting((i & 64) >> 6),
+                presoak = UserSetting((i & 256) >> 8),
                 lock_control = UserSetting((i & 512) >> 9),
-                sabbath = UserSetting((i & 64) >> 23),
+                dry_option = UserDryOptionSetting((i & 3072) >> 10),
+                wash_temp = UserWashTempSetting((i & 12288) >> 12),
+                delay_hours = (i & 983040) >> 16,
+                wash_zone = UserWashZoneSetting((i & 3145728) >> 20),
+                sound = UserSetting((i & 8388608) >> 23),
                 raw_value=value
             )
         except Exception as ex: 
--- gehomesdk/erd/values/dishwasher/erd_user_setting.py
+++ gehomesdk/erd/values/dishwasher/erd_user_setting.py
@@ -7,8 +7,43 @@ class UserSetting(enum.Enum):
     ENABLE = 1
     INVALID = 255
 
+@enum.unique
+class UserCycleSetting(enum.Enum):
+    AUTO = 0
+    INTENSE = 1
+    NORMAL = 2
+    DELICATE = 3
+    THIRTY_MIN = 4
+    ECO = 5
+    RINSE = 6
+
+@enum.unique
+class UserWashTempSetting(enum.Enum):
+    NORMAL = 0
+    BOOST = 1
+    SANITIZE = 2
+
+@enum.unique
+class UserDryOptionSetting(enum.Enum):
+    OFF = 0
+    POWER_DRY = 1
+    MAX_DRY = 2
+
+@enum.unique
+class UserWashZoneSetting(enum.Enum):
+    BOTH = 0
+    LOWER = 1
+    UPPER = 2
+
 class ErdUserSetting (NamedTuple):
     sound: UserSetting = UserSetting.DISABLE
     lock_control: UserSetting = UserSetting.DISABLE
     sabbath: UserSetting = UserSetting.DISABLE
+    cycle_mode: UserCycleSetting = UserCycleSetting.AUTO
+    presoak: UserSetting = UserSetting.DISABLE
+    bottle_jet: UserSetting = UserSetting.DISABLE
+    wash_temp: UserWashTempSetting = UserWashTempSetting.NORMAL
+    dry_option: UserDryOptionSetting = UserDryOptionSetting.OFF
+    wash_zone: UserWashZoneSetting = UserWashZoneSetting.BOTH
+    delay_hours: int = 0
     raw_value: Optional[str] = None
