b0d02d7678fd2553956c589d32fcc6a7c969d220
Jack Simbach <jack.simbach@gmail.com>
2021-07-21 15:36:21 -0400

    - updated remote delay start to timespan

--- gehomesdk/erd/erd_configuration.py
+++ gehomesdk/erd/erd_configuration.py
@@ -129,7 +129,7 @@ _configuration = [
     ErdConfigurationEntry(ErdCode.LAUNDRY_DOOR, LaundryDoorStatusConverter(), ErdCodeClass.DOOR),
     ErdConfigurationEntry(ErdCode.LAUNDRY_CYCLE, LaundryCycleConverter(), ErdCodeClass.LAUNDRY_SENSOR),
     ErdConfigurationEntry(ErdCode.LAUNDRY_REMOTE_STATUS, ErdReadOnlyBoolConverter(), ErdCodeClass.LAUNDRY_SENSOR),
-    ErdConfigurationEntry(ErdCode.LAUNDRY_REMOTE_DELAY_CONTROL, ErdIntConverter(), ErdCodeClass.LAUNDRY_SENSOR),
+    ErdConfigurationEntry(ErdCode.LAUNDRY_REMOTE_DELAY_CONTROL, ErdTimeSpanConverter(), ErdCodeClass.LAUNDRY_SENSOR),
     ErdConfigurationEntry(ErdCode.LAUNDRY_REMOTE_POWER_CONTROL, ErdIntConverter(length=1), ErdCodeClass.LAUNDRY_SENSOR),
 
     # Laundry - Washer
