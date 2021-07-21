0e313606fec0fc55e89d91a04fd5a6cc01c2be07
Jack Simbach <jack.simbach@gmail.com>
2021-07-21 11:07:23 -0400

    - added length parameter for timespan encoding - modified the default length for timespans to two bytes

--- gehomesdk/erd/converters/primitives/erd_time_span_converter.py
+++ gehomesdk/erd/converters/primitives/erd_time_span_converter.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from typing import Optional
 
 from ..abstract import ErdReadWriteConverter, ErdReadOnlyConverter
+from gehomesdk.erd.erd_codes import ErdCodeType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -14,21 +15,24 @@ def erd_decode_timespan(value: any) -> Optional[timedelta]:
         _LOGGER.debug('Got timespan value of 65535. Treating as None.')
         return None
     return timedelta(minutes=minutes)
-def erd_encode_timespan(value: Optional[timedelta]) -> str:
+def erd_encode_timespan(value: Optional[timedelta], length: int = 2) -> str:
     """ Encodes a time span as an erd integer (minutes), None is encoded as 65535. """
     if value is None:
         minutes = 65535
     else:
         minutes = value.seconds // 60
-    return minutes.to_bytes(4, 'big').hex()
+    return minutes.to_bytes(length, 'big').hex()
 
 class ErdTimeSpanConverter(ErdReadWriteConverter[Optional[timedelta]]):
+    def __init__(self, erd_code: ErdCodeType = "Unknown", length: int = 2):
+        super().__init__(erd_code)
+        self.length = length    
     def erd_decode(self, value: str) -> Optional[timedelta]:
         """ Decodes a raw integer as a time span, 65535 is treated as None. """
         return erd_decode_timespan(value)
     def erd_encode(self, value: Optional[timedelta]) -> str:
         """ Encodes a time span as an erd integer, None is encoded as 65535. """
-        return erd_encode_timespan(value)
+        return erd_encode_timespan(value, self.length)
 
 class ErdReadOnlyTimeSpanConverter(ErdReadOnlyConverter[Optional[timedelta]]):
     def erd_decode(self, value: str) -> Optional[timedelta]:
--- gehomesdk/erd/converters/primitives/erd_time_span_seconds_converter.py
+++ gehomesdk/erd/converters/primitives/erd_time_span_seconds_converter.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from typing import Optional
 
 from ..abstract import ErdReadWriteConverter, ErdReadOnlyConverter
+from gehomesdk.erd.erd_codes import ErdCodeType
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -14,21 +15,24 @@ def erd_decode_timespan_seconds(value: any) -> Optional[timedelta]:
         _LOGGER.debug('Got timespan value of 65535. Treating as None.')
         return None
     return timedelta(seconds=seconds)
-def erd_encode_timespan_seconds(value: Optional[timedelta]) -> str:
+def erd_encode_timespan_seconds(value: Optional[timedelta], length: int = 4) -> str:
     """ Encodes a time span as an erd integer (seconds) , None is encoded as 65535. """
     if value is None:
         seconds = 65535
     else:
         seconds = value.seconds
-    return seconds.to_bytes(4, 'big').hex()    
+    return seconds.to_bytes(length, 'big').hex()    
 
 class ErdTimeSpanSecondsConverter(ErdReadWriteConverter[Optional[timedelta]]):
+    def __init__(self, erd_code: ErdCodeType = "Unknown", length: int = 4):
+        super().__init__(erd_code)
+        self.length = length        
     def erd_decode(self, value: str) -> Optional[timedelta]:
         """ Decodes a raw integer (seconds) as a time span, 65535 is treated as None. """
         return erd_decode_timespan_seconds(value)
     def erd_encode(self, value: Optional[timedelta]) -> str:
         """ Encodes a time span as an erd integer (seconds), None is encoded as 65535. """
-        return erd_encode_timespan_seconds(value)
+        return erd_encode_timespan_seconds(value, self.length)
 
 class ErdReadOnlyTimeSpanSecondsConverter(ErdReadOnlyConverter[Optional[timedelta]]):
     def erd_decode(self, value: str) -> Optional[timedelta]:
