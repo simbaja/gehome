d88accdb391ed7c240fa66a5df33c2526ed5b840
Jack Simbach <jack.simbach@gmail.com>
2021-08-04 17:47:14 -0400

    - bug fixes for split ac

--- gehomesdk/erd/converters/ac/erd_sac_available_modes_converter.py
+++ gehomesdk/erd/converters/ac/erd_sac_available_modes_converter.py
@@ -7,6 +7,6 @@ class ErdSacAvailableModesConverter(ErdReadOnlyConverter[ErdSacAvailableModes]):
     def erd_decode(self, value: str) -> ErdSacAvailableModes:
         try:
             modes = erd_decode_int(value)
-            return ErdSacAvailableModes(modes & 1, modes & 2, modes & 4, value)
+            return ErdSacAvailableModes(modes & 1 == 1, modes & 2 == 2, modes & 4 == 4, value)
         except:
             return ErdSacAvailableModes(False, False, False, value)
--- gehomesdk/erd/values/ac/sac_enums.py
+++ gehomesdk/erd/values/ac/sac_enums.py
@@ -12,6 +12,7 @@ class ErdSacAvailableModes(NamedTuple):
 class ErdSacTargetTemperatureRange(NamedTuple):
     min: int
     max: int
+    raw_value: str
 
     def stringify(self, **kwargs) -> Optional[str]:
         return f"min: {self.min}, max: {self.max}"
