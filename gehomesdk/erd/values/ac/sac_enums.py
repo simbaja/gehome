ce0cf8d645daeb45333c51a816a6aaca2daa6dab
Jack Simbach <jack.simbach@gmail.com>
2021-08-08 11:40:15 -0400

    - fixed interpretation of SAC mode

--- gehomesdk/erd/values/ac/sac_enums.py
+++ gehomesdk/erd/values/ac/sac_enums.py
@@ -3,11 +3,11 @@ from typing import Optional, NamedTuple
 class ErdSacAvailableModes(NamedTuple):
     has_heat: bool
     has_dry: bool
-    has_auto: bool
+    has_eco: bool
     raw_value: str
 
     def stringify(self, **kwargs) -> Optional[str]:
-        return f"heat: {self.has_heat}, dry: {self.has_dry}, auto: {self.has_auto}"
+        return f"heat: {self.has_heat}, dry: {self.has_dry}, eco: {self.has_eco}"
 
 class ErdSacTargetTemperatureRange(NamedTuple):
     min: int
