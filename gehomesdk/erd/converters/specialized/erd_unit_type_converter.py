6cd20a9e60e7bd4d38d9e3b2f288b44f5bbcdb26
Jack Simbach <jack.simbach@gmail.com>
2021-08-10 13:22:06 -0400

    - fix for unknown unit types

--- gehomesdk/erd/converters/specialized/erd_unit_type_converter.py
+++ gehomesdk/erd/converters/specialized/erd_unit_type_converter.py
@@ -1,7 +1,17 @@
+import logging
+
+
+import logging
 from ..abstract import ErdReadOnlyConverter
 from ..primitives import *
 from gehomesdk.erd.values.common import ErdUnitType
 
+_LOGGER = logging.getLogger(__name__)
+
 class ErdUnitTypeConverter(ErdReadOnlyConverter[ErdUnitType]):
     def erd_decode(self, value: str) -> ErdUnitType:
-        return ErdUnitType(erd_decode_int(value))
+        try:
+            return ErdUnitType(erd_decode_int(value))
+        except:
+            _LOGGER.warning("Unknown Unit Type: {value}, using default")
+            return ErdUnitType.UNKNOWN
