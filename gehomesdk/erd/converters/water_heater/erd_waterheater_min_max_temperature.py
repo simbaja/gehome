d13d226128f8c9a43a2b8afee4a85e6d70786857
Sean Tibor <40579452+seantibor@users.noreply.github.com>
2022-09-06 08:59:13 -0400

    use erd_decode_bytes

--- gehomesdk/erd/converters/water_heater/erd_waterheater_min_max_temperature.py
+++ gehomesdk/erd/converters/water_heater/erd_waterheater_min_max_temperature.py
@@ -8,7 +8,7 @@ class ErdWaterHeaterMinMaxTemperatureConverter(
     ErdReadOnlyConverter[ErdWaterHeaterMinMaxTemperature]
 ):
     def erd_decode(self, value: str) -> ErdWaterHeaterMinMaxTemperature:
-        raw_bytes = bytes.fromhex(value)
+        raw_bytes = erd_decode_bytes(value)
         lower_temp = int.from_bytes(raw_bytes[:2], "big") / 10
         upper_temp = int.from_bytes(raw_bytes[-2:], "big") / 10
         return ErdWaterHeaterMinMaxTemperature(lower=lower_temp, upper=upper_temp)
