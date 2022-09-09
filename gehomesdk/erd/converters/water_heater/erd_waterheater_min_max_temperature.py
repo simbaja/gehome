2d32b5daae7a39ec6e2d04628481e470736ddc35
Sean Tibor <40579452+seantibor@users.noreply.github.com>
2022-09-09 14:34:33 -0400

    improved temperature decode

--- gehomesdk/erd/converters/water_heater/erd_waterheater_min_max_temperature.py
+++ gehomesdk/erd/converters/water_heater/erd_waterheater_min_max_temperature.py
@@ -7,8 +7,21 @@ from ..primitives import *
 class ErdWaterHeaterMinMaxTemperatureConverter(
     ErdReadOnlyConverter[ErdWaterHeaterMinMaxTemperature]
 ):
+
+    ERD_WATERHEATER_TEMPERATURE_MIN = 100
+    ERD_WATERHEATER_TEMPERATURE_MAX = 140
+
     def erd_decode(self, value: str) -> ErdWaterHeaterMinMaxTemperature:
-        raw_bytes = erd_decode_bytes(value)
-        lower_temp = int.from_bytes(raw_bytes[:2], "big") / 10
-        upper_temp = int.from_bytes(raw_bytes[-2:], "big") / 10
+
+        try:
+            lower_temp = erd_decode_int(value[:4]) / 10
+            upper_temp = erd_decode_int(value[-4:]) / 10
+        except ValueError:
+            return ErdWaterHeaterMinMaxTemperature(
+                self.ERD_WATERHEATER_TEMPERATURE_MIN,
+                self.ERD_WATERHEATER_TEMPERATURE_MAX,
+            )
+        if lower_temp == 0 or upper_temp == 0:
+            lower_temp = self.ERD_WATERHEATER_TEMPERATURE_MIN
+            upper_temp = self.ERD_WATERHEATER_TEMPERATURE_MAX
         return ErdWaterHeaterMinMaxTemperature(lower=lower_temp, upper=upper_temp)
