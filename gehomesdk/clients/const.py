5573238e71657a7633b8f8f840962c9ca9abe0e0
jtbnz <57672422+jtbnz@users.noreply.github.com>
2025-04-14 16:29:36 +1200

    Remove field trial for OAuth2 credentials

--- gehomesdk/clients/const.py
+++ gehomesdk/clients/const.py
@@ -1,23 +1,13 @@
 """Constants"""
 
 # OAuth2 credentials
-FIELD_TRIAL = True
+OAUTH2_APP_ID = "com.ge.kitchen.wca.prd.android"
+OAUTH2_CLIENT_ID = "564c31616c4f7474434b307435412b4d2f6e7672"
+OAUTH2_CLIENT_SECRET = "6476512b5246446d452f697154444941387052645938466e5671746e5847593d"
+OAUTH2_REDIRECT_URI = "brillion.4e617a766474657344444e562b5935566e51324a://oauth/redirect"
 
-if FIELD_TRIAL:
-  OAUTH2_APP_ID = "com.ge.kitchen.wca.prd.android"
-  OAUTH2_CLIENT_ID= "55527330624d73306a63565a554f612b337a3842"
-  OAUTH2_CLIENT_SECRET = "714c6574425736715763426e35554778625351684b4f654f3847654d7343733d"
-  OAUTH2_REDIRECT_URI ="brillion.6c454f4c53673173385179514c4348616e452b63://oauth/redirect"
-  LOGIN_URL = "https://accounts-fld.brillion.geappliances.com"
-  API_URL = "https://api-fld.brillion.geappliances.com"
-else:
-  OAUTH2_APP_ID = "com.ge.kitchen.wca.prd.android"
-  OAUTH2_CLIENT_ID = "564c31616c4f7474434b307435412b4d2f6e7672"
-  OAUTH2_CLIENT_SECRET = "6476512b5246446d452f697154444941387052645938466e5671746e5847593d"
-  OAUTH2_REDIRECT_URI = "brillion.4e617a766474657344444e562b5935566e51324a://oauth/redirect"
-  LOGIN_URL = "https://accounts.brillion.geappliances.com"
-  API_URL = "https://api.brillion.geappliances.com"
-
+LOGIN_URL = "https://accounts.brillion.geappliances.com"
+API_URL = "https://api.brillion.geappliances.com"
 SECURE_URL = "https://secure.brillion.geappliances.com"
 
 LOGIN_REGIONS = {
