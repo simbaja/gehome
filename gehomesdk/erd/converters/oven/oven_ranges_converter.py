52fb86f1bdbaf96c709eb59abcd80392d473224a
ehendrix23 <hendrix_erik@hotmail.com>
2025-10-06 17:48:13 -0600

    Fix debug log formatting for temperature values

--- gehomesdk/erd/converters/oven/oven_ranges_converter.py
+++ gehomesdk/erd/converters/oven/oven_ranges_converter.py
@@ -10,7 +10,7 @@ class OvenRangesConverter(ErdReadOnlyConverter[OvenRanges]):
         raw_bytes = bytes.fromhex(value)
         upper_temp = int.from_bytes(raw_bytes[:2], 'big')
         lower_temp = int.from_bytes(raw_bytes[-2:], 'big')
-        _LOGGER.debug("Received value: '%s', translated to upper_temp: '%s' and lower_temp '%'",value,upper_temp,lower_temp)
+        _LOGGER.debug("Received value: '%s', translated to upper_temp: '%s' and lower_temp '%s'",value,upper_temp,lower_temp)
         return OvenRanges(
             lower=lower_temp, 
             upper=upper_temp
