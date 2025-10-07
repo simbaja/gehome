e961108bcda046cf657176b07589ab9dc0686620
ehendrix23 <hendrix_erik@hotmail.com>
2025-10-07 11:50:03 -0600

    Implement logging in kitchen timer min/max converter
    Added logging to debug kitchen timer values during decoding.

--- gehomesdk/erd/converters/advantium/erd_advantium_kitchen_timer_min_max_converter.py
+++ gehomesdk/erd/converters/advantium/erd_advantium_kitchen_timer_min_max_converter.py
@@ -1,11 +1,17 @@
+import logging
 from ..abstract import ErdReadOnlyConverter
 from ..primitives import *
 from gehomesdk.erd.values.advantium import ErdAdvantiumKitchenTimerMinMax
 
+_LOGGER = logging.getLogger(__name__)
+
 class ErdAdvantiumKitchenTimerMinMaxConverter(ErdReadOnlyConverter[ErdAdvantiumKitchenTimerMinMax]):
     def erd_decode(self, value: str) -> ErdAdvantiumKitchenTimerMinMax:
-        return ErdAdvantiumKitchenTimerMinMax(
+        kitchen_timer_min_max = ErdAdvantiumKitchenTimerMinMax(
             min_time=erd_decode_timespan(value[0:4], uom="seconds"),
             max_time=erd_decode_timespan(value[4:8], uom="seconds"),
             raw_value=value
         )
+        _LOGGER.debug("Kitchen Timer for value %s is: %s", value, kitchen_timer_min_max)
+        return kitchen_timer_min_max
+        
