9856902d962ae48bfe0063b4603a27ff410df476
Jack Simbach <jack.simbach@gmail.com>
2025-11-15 21:14:07 -0500

    - improved shutdown for example and gather_data scripts

--- examples/websocket_example.py
+++ examples/websocket_example.py
@@ -5,11 +5,13 @@ We're going to run the client in a pre-existing event loop.  We're also going to
 to update appliances every five minutes and to turn on our oven the first time we see it.  Because that is safe!
 """
 
+import platform
 import aiohttp
 import asyncio
 import logging
+import signal
 from datetime import timedelta
-from typing import Any, Dict, Tuple
+from typing import Any, Dict, Tuple, Optional
 from credentials import USERNAME, PASSWORD, REGION
 
 from gehomesdk import (
@@ -39,19 +41,18 @@ async def detect_appliance_type(appliance: GeAppliance):
     Detect the appliance type.
     This should only be triggered once since the appliance type should never change.
 
-    Also, let's turn on ovens!
+    Also, let's set a timer!
     """
     _LOGGER.debug(f'Appliance state change detected in {appliance}')
     if appliance.appliance_type == ErdApplianceType.OVEN:
-        _LOGGER.info('Turning on the oven!')
-        await appliance.async_set_erd_value(
-            ErdCode.UPPER_OVEN_COOK_MODE,
-            OvenCookSetting(OVEN_COOK_MODE_MAP[ErdOvenCookMode.BAKE_NOOPTION], 350)
-        )
-        _LOGGER.info('Set the timer!')
+        # _LOGGER.info('Turning on the oven!')
+        # await appliance.async_set_erd_value(
+        #     ErdCode.UPPER_OVEN_COOK_MODE,
+        #     OvenCookSetting(OVEN_COOK_MODE_MAP[ErdOvenCookMode.BAKE_NOOPTION], 350)
+        # )
+        _LOGGER.info('Set the oven timer!')
         await appliance.async_set_erd_value(ErdCode.UPPER_OVEN_KITCHEN_TIMER, timedelta(minutes=45))
         pass
-
 
 async def do_periodic_update(appliance: GeAppliance):
     """Request a full state update every minute forever"""
@@ -61,21 +62,55 @@ async def do_periodic_update(appliance: GeAppliance):
         _LOGGER.debug(f'Requesting update for {appliance:s}')
         await appliance.async_request_update()
 
+async def shutdown(sig, client: Optional[GeWebsocketClient], client_task: Optional[asyncio.Task[None]]):
+    #just exit if we don't have a client or task
+    if client is None or client_task is None:
+        return
+
+    _LOGGER.info(f"Signal caught: {sig}, shutting down...")
+
+    # create the group to await
+    group = asyncio.gather(client_task)
+    
+    try:
+        #disconnect and wait a bit for it to shutdown gracefully
+        await client.disconnect()
+        _ , _ = await asyncio.wait([client_task], return_when=asyncio.FIRST_COMPLETED, timeout=10)
+    except TimeoutError:
+        _LOGGER.warning("Client did not shut down gracefully, forcing shutdown.")
+        client_task.cancel()
+
+    await group
+
+    _LOGGER.info(f"Shutdown complete.")
 
 async def main():
-    client = GeWebsocketClient(USERNAME, PASSWORD, REGION)
-    client.add_event_handler(EVENT_APPLIANCE_INITIAL_UPDATE, detect_appliance_type)
-    client.add_event_handler(EVENT_APPLIANCE_STATE_CHANGE, log_state_change)
-    client.add_event_handler(EVENT_ADD_APPLIANCE, do_periodic_update)
+    loop = asyncio.get_running_loop()
+    client: GeWebsocketClient | None = None
+    client_task: asyncio.Task[None] | None = None
 
-    async with aiohttp.ClientSession() as session:
-        client_task = asyncio.create_task(client.async_get_credentials_and_run(session))
-        await asyncio.sleep(7400)
-        client_task.cancel()
-        try:
-            await client_task
-        except asyncio.CancelledError:
-            pass
+    try:
+        client = GeWebsocketClient(USERNAME, PASSWORD, REGION)
+        client.add_event_handler(EVENT_APPLIANCE_INITIAL_UPDATE, detect_appliance_type)
+        client.add_event_handler(EVENT_APPLIANCE_STATE_CHANGE, log_state_change)
+        client.add_event_handler(EVENT_ADD_APPLIANCE, do_periodic_update)
+
+        async with aiohttp.ClientSession() as session:
+            # start up our client
+            client_task = asyncio.create_task(client.async_get_credentials_and_run(session))
+
+            # setup signal handlers to catch termination signals (only linux) 
+            if platform.system() != "Windows":
+                for s in [signal.SIGINT, signal.SIGTERM]:
+                    loop.add_signal_handler(s, lambda s=s: asyncio.create_task(shutdown(s, client, client_task)))
+
+            # wait for a while...
+            await asyncio.sleep(7200)
+    except asyncio.exceptions.CancelledError:
+        pass
+    finally:
+        #shutdown gracefully if possible
+        await shutdown(signal.SIGTERM, client, client_task)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)-8s %(message)s')
--- gehomesdk/gather_data.py
+++ gehomesdk/gather_data.py
@@ -5,7 +5,9 @@ Gets the appliance data, continues to run until cancelled so that values can be 
 import aiohttp
 import asyncio
 import logging
-from typing import Any, Dict, Tuple
+import platform
+import signal
+from typing import Any, Dict, Tuple, Optional
 
 from gehomesdk import (
     EVENT_ADD_APPLIANCE,
@@ -28,8 +30,6 @@ async def detect_appliance_type(appliance: GeAppliance):
     """
     Detect the appliance type.
     This should only be triggered once since the appliance type should never change.
-
-    Also, let's turn on ovens!
     """
     _LOGGER.debug(f'Appliance state change detected in {appliance}')
 
@@ -41,19 +41,56 @@ async def do_periodic_update(appliance: GeAppliance):
         _LOGGER.debug(f'Requesting update for {appliance:s}')
         await appliance.async_request_update()
 
+async def shutdown(sig, client: Optional[GeWebsocketClient], client_task: Optional[asyncio.Task[None]]):
+    #just exit if we don't have a client or task
+    if client is None or client_task is None:
+        return
+
+    _LOGGER.info(f"Signal caught: {sig}, shutting down...")
+
+    # create the group to await
+    group = asyncio.gather(client_task)
+    
+    try:
+        #disconnect and wait a bit for it to shutdown gracefully
+        await client.disconnect()
+        _ , _ = await asyncio.wait([client_task], return_when=asyncio.FIRST_COMPLETED, timeout=10)
+    except TimeoutError:
+        _LOGGER.warning("Client did not shut down gracefully, forcing shutdown.")
+        client_task.cancel()
+
+    await group
+
+    _LOGGER.info(f"Shutdown complete.")        
+
 def gather_appliance_data(username: str, password: str, region: str):
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)-8s %(message)s')
     asyncio.run(async_gather_appliance_data(username, password, region))
 
 async def async_gather_appliance_data(username: str, password: str, region: str):
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)-8s %(message)s')
+    loop = asyncio.get_running_loop()
+    client: GeWebsocketClient | None = None
+    client_task: asyncio.Task[None] | None = None
 
-    loop = asyncio.get_event_loop()
+    try:
+        client = GeWebsocketClient(username, password, region)
+        client.add_event_handler(EVENT_APPLIANCE_INITIAL_UPDATE, detect_appliance_type)
+        client.add_event_handler(EVENT_APPLIANCE_STATE_CHANGE, log_state_change)
+        client.add_event_handler(EVENT_ADD_APPLIANCE, do_periodic_update)
 
-    client = GeWebsocketClient(username, password, region, loop)
-    client.add_event_handler(EVENT_APPLIANCE_INITIAL_UPDATE, detect_appliance_type)
-    client.add_event_handler(EVENT_APPLIANCE_STATE_CHANGE, log_state_change)
-    client.add_event_handler(EVENT_ADD_APPLIANCE, do_periodic_update)
+        async with aiohttp.ClientSession() as session:
+            # start up our client
+            client_task = asyncio.create_task(client.async_get_credentials_and_run(session))
 
-    session = aiohttp.ClientSession()
-    asyncio.ensure_future(client.async_get_credentials_and_run(session), loop=loop)
-    await asyncio.sleep(7400)
+            # setup signal handlers to catch termination signals (only linux)
+            if platform.system() != "Windows":
+                for s in [signal.SIGINT, signal.SIGTERM]:
+                    loop.add_signal_handler(s, lambda s=s: asyncio.create_task(shutdown(s, client, client_task)))
+
+            # wait for a while...
+            await asyncio.sleep(7200)
+    except asyncio.exceptions.CancelledError:
+        pass
+    finally:
+        #shutdown gracefully if possible
+        await shutdown(signal.SIGTERM, client, client_task)
